3cc313b97441edb759d13613681113291f2c4b22
dang-gun <c1dong8@gmail.com>
2019-09-23 02:56:19 +0900

    Add files via upload

--- DG_JsDataBind/test/index.js
+++ DG_JsDataBind/test/index.js
@@ -11,6 +11,24 @@ var sOriData = "test001 => {{test001}}, \n"
     + "test001:test1 => {{test001:test1}}\n"
     + "test002:test2 => {{test002:test2}}\n";
 
+function OnStart00() {
+    //기본정 샘플
+
+    var sOriData_InData = domTxtInData.value;
+
+    var sReturn =
+        insDB.DataBind_SelectType(
+            sOriData_InData
+            , ["defult"]
+            , {
+                "test001": "test-001"
+            }
+        );
+
+    domTxtResult.value = sReturn;
+
+}
+
 function OnStart01()
 {
     //매치 리스트 지정 샘플
@@ -20,7 +38,7 @@ function OnStart01()
     var sReturn = 
         insDB.DataBind_SelectType(
             sOriData_InData
-            , insDB.ListMatchList["defult"]
+            , insDB.MatchPatternList["defult"]
             , {
                 "test001": "test-001"
                 , "test002": "test-002"
@@ -38,13 +56,13 @@ function OnStart02()
 
     var sOriData_InData = domTxtInData.value;
 
-    insDB.MatchListAdd("AddTest"
+    insDB.MatchPatternListAdd("AddTest"
         , {
             ":test1": function (sOriData, sMatchString, sValue) { return insDB.ReplaceAll(sOriData, sMatchString, sValue + ":AddTest1"); }
             , ":test2": function (sOriData, sMatchString, sValue) { return insDB.ReplaceAll(sOriData, sMatchString, sValue + ":AddTest1"); }
         });
 
-
+    
 
     var arrTypeName = ["defult", "AddTest"];
         
@@ -59,5 +77,105 @@ function OnStart02()
             }
         );
 
+    domTxtResult.value = sReturn;
+}
+
+/** 셈플1 */
+function Test_Sample01()
+{
+    //개체 생성
+    var insDB = new DG_JsDataBind();
+    //원본 데이터
+    var sOriData = "내 나이는 {{age}}입니다.";
+
+    //바인딩에 사용할 데이터
+    var jsonValue = {
+        "name": "John"
+        , "age": "50"
+    };
+
+    //데이터 바인딩 시도
+    var sReturn =
+        insDB.DataBind(
+            sOriData
+            , ["defult"]
+            , jsonValue
+        );
+
+    //결과 출력
+    domTxtResult.value = sReturn;
+}
+
+
+
+/** 셈플2 */
+function Test_Sample02()
+{
+    //개체 생성
+    var insDB = new DG_JsDataBind();
+    //원본 데이터
+    var sOriData = "내 이름은 {{name:name1}}{{name:name2}} 나이는 {{age}}입니다.";
+
+    //사용자 정의 패턴 추가
+    var jsonAdd = {
+        ":name1": function (sOriData, sMatchString, sValue) { return insDB.ReplaceAll(sOriData, sMatchString, sValue); }
+        , ":name2": function (sOriData, sMatchString, sValue) { return insDB.ReplaceAll(sOriData, sMatchString, "(" + sValue + ")"); }
+    };
+
+    //패턴 추가
+    insDB.MatchPatternListAdd("NameAdd", jsonAdd);
+
+    //바인딩에 사용할 데이터
+    var jsonValue = {
+        "name": "John"
+        , "age": "50"
+    };
+
+
+    //데이터 바인딩 시도
+    var sReturn =
+        insDB.DataBind(
+            sOriData
+            , ["NameAdd"]
+            , jsonValue
+        );
+
+    //결과 출력
+    domTxtResult.value = sReturn;
+}
+
+
+/** 셈플2 */
+function Test_Sample03() {
+    //개체 생성
+    var insDB = new DG_JsDataBind();
+    //원본 데이터
+    var sOriData = "내 이름은 {{name:name1}}{{name:name2}} 나이는 {{age}}입니다.";
+
+    //사용자 정의 패턴 추가
+    var jsonAdd = {
+        ":name1": function (sOriData, sMatchString, sValue) { return insDB.ReplaceAll(sOriData, sMatchString, sValue); }
+        , ":name2": function (sOriData, sMatchString, sValue) { return insDB.ReplaceAll(sOriData, sMatchString, "(" + sValue + ")"); }
+    };
+
+    //패턴 추가
+    insDB.MatchPatternListAdd("NameAdd", jsonAdd);
+
+    //바인딩에 사용할 데이터
+    var jsonValue = {
+        "name": "John"
+        , "age": "50"
+    };
+
+
+    //데이터 바인딩 시도
+    var sReturn =
+        insDB.DataBind(
+            sOriData
+            , ["defult", "NameAdd"]
+            , jsonValue
+        );
+
+    //결과 출력
     domTxtResult.value = sReturn;
 }
