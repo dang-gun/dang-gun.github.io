e858b0baf5c45ab039c5c6fb88a9256efff3d1e4
dang-gun <c1dong8@gmail.com>
2020-02-28 16:49:43 +0900

    Add files via upload

---
+++ DG_JavaScript_Utility/DG_MessageBox/DG_MessageBox.js
@@ -0,0 +1,155 @@
+﻿/** DG_Popup을 활용한 메시지박스  */
+var DG_MessageBox = {};
+
+/** 사용할 div */
+DG_MessageBox.divMessageBox = null;
+
+/** 선택된 버튼의 타입 */
+DG_MessageBox.ButtonShowType = {
+    None: 0,
+    Ok: 1,
+    OkCancel: 2,
+    Cancel: 3,
+    YesNo: 4
+};
+
+DG_MessageBox.ButtonType = {
+    Ok: 0,
+    Cancel: 1,
+    Yes:2,
+    No: 3
+};
+
+DG_MessageBox.BigIconType = {
+    None: 0,
+    Info: 1,
+    Warning: 2,
+    Error: 3
+};
+
+
+/**
+ * DG_JQuery_Popup를 초기화 한다.
+ */
+DG_MessageBox.Initialize = function ()
+{
+    /** DG_Popup 생성 */
+    DG_Popup.Initialize();
+
+};
+
+/**
+ * 메시지 박스를 표시한다.
+ * @param {json} jsonOption 창옵션
+ */
+DG_MessageBox.Show = function (jsonOption)
+{
+    var jsonOptDefault = {
+        //제목
+        Title: "",
+        //내용
+        Content: "",
+
+        //버튼 타입
+        ButtonType: DG_MessageBox.ButtonShowType.Ok,
+        //큰 아이콘 타입
+        BigIconType: DG_MessageBox.BigIconType.None,
+
+        //버튼 이벤트
+        //function (DG_MessageBox.ButtonType)
+        //DG_MessageBox.ButtonType : 클릭된 버튼 정보
+        ButtonEvent: null,
+
+        //컨탠츠에 적용할 css
+        ContentCss: "DG_MessageBoxContentCss"
+    };
+
+    //완성된 오브젝트
+    var jsonOpt = Object.assign(jsonOptDefault, jsonOption);
+
+    //오브젝트를 추가할 비어 있는 개체
+    var divHtml = $(document.createDocumentFragment());
+
+    //타이틀
+    var divTitle = $("<div class='DG_PopupTitle DG_MessageBoxTitle'>" + jsonOpt.Title + "</div>");
+
+    //컨탠츠
+    var divContent = $("<div class='DG_MessageBoxContent'></div>");
+    //컨탠츠 - 큰 아이콘
+    switch (jsonOpt.BigIconType)
+    {
+        case DG_MessageBox.BigIconType.Info:
+            divContent.append("<div class='DG_MessageBoxBigIcon_info'></div>");
+            break;
+        case DG_MessageBox.BigIconType.Warning:
+            divContent.append("<div class='DG_MessageBoxBigIcon_Warning'></div>");
+            break;
+        case DG_MessageBox.BigIconType.Error:
+            divContent.append("<div class='DG_MessageBoxBigIcon_Error'></div>");
+            break;
+    }
+
+    //컨탠츠 - html 내용
+    var divContentHtml = $("<div class='DG_MessageBoxContentHtml'></div>");
+    divContentHtml.html(jsonOpt.Content);
+    divContent.append(divContentHtml);
+    
+
+    //푸터
+    var divFooter = $("<div class='DG_MessageBoxFooter'></div>");
+
+    if (jsonOpt.ButtonType === DG_MessageBox.ButtonShowType.Ok
+        || jsonOpt.ButtonType === DG_MessageBox.ButtonShowType.OkCancel)
+    {//ok 버튼
+        //ok cancel
+        divFooter.append("<button>OK</button>");
+        if (typeof jsonOpt.ButtonEvent === "function")
+        {
+            divFooter.find("button").click(function ()
+            {
+                jsonOpt.ButtonEvent(DG_MessageBox.ButtonType.Ok);
+            });
+        }
+    }
+
+    if (jsonOpt.ButtonType === DG_MessageBox.ButtonShowType.OkCancel
+        || jsonOpt.ButtonType === DG_MessageBox.ButtonShowType.Cancel)
+    {//ok 버튼
+        divFooter.append("<button>Cancel</button>");
+        if (typeof jsonOpt.ButtonEvent === "function")
+        {
+            divFooter.find("button").click(function ()
+            {
+                jsonOpt.ButtonEvent(DG_MessageBox.ButtonType.Cancel);
+            });
+        }
+    }
+
+    if (jsonOpt.ButtonType === DG_MessageBox.ButtonShowType.YesNo)
+    {//YesNo 버튼
+        divFooter.append($("<button>Yes</button>"));
+        divFooter.append("<button>No</button>");
+
+        if (typeof jsonOpt.ButtonEvent === "function")
+        {
+            var btn = divFooter.find("button");
+            $(btn[0]).click(function ()
+            {
+                jsonOpt.ButtonEvent(DG_MessageBox.ButtonType.Yes);
+            });
+            $(btn[1]).click(function ()
+            {
+                jsonOpt.ButtonEvent(DG_MessageBox.ButtonType.No);
+            });
+        }
+    }
+
+    //html 완성하기
+    divHtml.append(divTitle);
+    divHtml.append(divContent);
+    divHtml.append(divFooter);
+
+    jsonOpt.Content = divHtml;
+
+    DG_Popup.Show(jsonOpt);
+};
---
+++ DG_JavaScript_Utility/DG_Popup/DG_Popup.js
@@ -0,0 +1,281 @@
+﻿/**
+ * Multiple analog
+ * 다중 다얄로그
+ * 다중 팝업 라이브러리
+ * 
+ * 
+ * */
+var DG_Popup = {};
+
+/** z인덱스 시작값 */
+DG_Popup.ZindexStart = 1000;
+/** 다음 창의 z인덱스 추가 값 */
+DG_Popup.ZindexAdd = 10;
+
+
+/** 생성된 팝업의 고유번호를 발행용 */
+DG_Popup.PopupIndex = 0;
+
+/** 현재 선택된 인덱스 */
+DG_Popup.SelectIndex = 0;
+/** 현재 선택된 팝업 */
+DG_Popup.SelectDiv = null;
+
+/** 0: 없음, 1:다운, 2:업 */
+DG_Popup.MouseState = 0;
+/** 마우스 다운 계산값 X */
+DG_Popup.MouseDownX = 0;
+/** 마우스 다운 계산값 Y */
+DG_Popup.MouseDownY = 0;
+
+/** 창이 생성되면 쌓일 배열 */
+DG_Popup.List = [];
+
+/** DG_Popup를 초기화 한다.  */
+DG_Popup.Initialize = function ()
+{
+    $(document).on("mousedown", "div.DG_PopupTitle", function (e)
+    {
+        //바로 상위 div를 찾는다.
+        DG_Popup.SelectDiv = $(this).parent();
+        DG_Popup.SelectIndex = DG_Popup.SelectDiv.attr("popupIndex");
+
+        //마우스 상태 변경
+        DG_Popup.MouseState = 1;
+        //마우스 다운 계산값
+        DG_Popup.MouseDownX = e.clientX - DG_Popup.CutBack(DG_Popup.SelectDiv.css("left"));
+        DG_Popup.MouseDownY = e.clientY - DG_Popup.CutBack(DG_Popup.SelectDiv.css("top"));
+
+    });
+
+    $(document).on("mousemove", "div.DG_PopupTitle", function (e)
+    {
+        if (1 === DG_Popup.MouseState)
+        {//마우스 다운 상태
+            //창위치 변경
+            DG_Popup.SelectDiv.css("left", e.clientX - DG_Popup.MouseDownX);
+            DG_Popup.SelectDiv.css("top", e.clientY - DG_Popup.MouseDownY);
+        }
+    });
+
+    $(document).on("mouseup", "div.DG_PopupTitle", function (e)
+    {
+        //마우스 상태 변경
+        DG_Popup.MouseState = 2;
+    });
+};
+
+/**
+ * DG_JQuery_Popup를 만들어서 표시한다.
+ * @param {json} jsonOption 창옵션
+ * @returns {object} 만들어진 팝업 오브젝트
+ */
+DG_Popup.Show = function (jsonOption)
+{
+    var jsonOptDefault = {
+        /** 팝업 안에 표시할 컨탠츠
+         * 오브젝트도 가능하다. */
+        Content: "",
+        /** 컨탠츠에 적용할 css */
+        ContentCss: "",
+
+        /** 시작위치 - Y */
+        top: 0,
+        /** 시작위치 - X */
+        left: 0,
+        /** 가로 크기 */
+        width: "auto",
+        /** 세로 크기 */
+        height: "auto",
+
+        /** 부모에 적용할 css */
+        ParentCss: "",
+
+        /** 배경색 */
+        ContentBackground: "#fff",
+
+        /**
+         * 오버레이 클릭시 사용할 이벤트
+         * null이면 오버레이를 클릭해도 동작하지 않는다.
+         * 창을 닫으려면 'DG_Popup.CloseTarget(divPopupParent);'를 넣는다.
+         * 
+         * function (nPopupIndex, divPopupParent)
+         * nPopupIndex : 생성에 사용된 인덱스
+         * divPopupParent : 생성된 창의 개체 
+         * */
+        OverlayClick: null,
+        /** 오버레이용 배경색 */
+        OverlayBackground: "#aaaaaa",
+        /** 오버레이 불투명 값 */
+        OverlayOpacity: 0.3,
+        /** 오버레이에 적용할 css */
+        OverlayCss: ""
+    };
+
+    //옵션 합치기
+    var jsonOpt = Object.assign(jsonOptDefault, jsonOption);
+
+    //고유키 증가;
+    var nPopupIndex = ++DG_Popup.PopupIndex;
+
+    //사용할 z인덱스 계산
+    var nZIdx = DG_Popup.ZindexStart + (DG_Popup.ZindexAdd * DG_Popup.List.length);
+
+    //부모용 div*************
+    var divPopupParent = $("<div id='divDG_PopupParent" + nPopupIndex
+        + "' class='DG_PopupParentCss' popupIndex='" + nPopupIndex + "'></div>");
+    //사용자 정의 css 추가
+    divPopupParent.addClass(jsonOpt.ParentCss);
+
+    //오버레이용 div*************
+    var divOverlay = $("<div id='divDG_PopupOverlay" + nPopupIndex + "' class='DG_PopupOverlayCss'></div>");
+    divOverlay.addClass(jsonOpt.OverlayCss);
+    divOverlay.css("background", jsonOpt.OverlayBackground);
+    divOverlay.css("opacity", jsonOpt.OverlayOpacity);
+
+    divOverlay.css("position", "fixed");
+    divOverlay.css("top", "0px");
+    divOverlay.css("left", "0px");
+    divOverlay.css("width", "100%");
+    divOverlay.css("height", "100%");
+
+    divOverlay.css("z-index", nZIdx);
+
+    
+    
+
+    //컨탠츠용 div*************
+    var divContent = $("<div id='divDG_Popup" + nPopupIndex
+        + "' class='DG_PopupContentCss' popupIndex='" + nPopupIndex + "'></div>");
+    //사용자 정의 css 추가
+    divContent.addClass(jsonOpt.ContentCss);
+    divContent.css("position", "absolute");
+    divContent.css("background", jsonOpt.ContentBackground);
+
+    divContent.css("top", jsonOpt.top + "px");
+    divContent.css("left", jsonOpt.left + "px");
+    divContent.css("width", jsonOpt.width);
+    divContent.css("height", jsonOpt.height);
+
+    divContent.css("z-index", nZIdx + 1 );
+
+    //html 출력
+    if (typeof jsonOpt.Content === "string")
+    {//문자열인 경우
+        divContent.html(jsonOpt.Content);
+    }
+    else if (typeof jsonOpt.Content === "object")
+    {//오브젝트인 경우
+        divContent.append(jsonOpt.Content);
+    }
+    
+
+    //부모 div에 추가 한다.
+    divPopupParent.append(divContent);
+    divPopupParent.append(divOverlay);
+    //바디 끝에 사용할 div를 만들어 준다.
+    $("body").append(divPopupParent);
+
+    //새로 추가한 개체를 찾는다.
+    var divPopupParent_New = $("#divDG_PopupParent" + nPopupIndex);
+    var divOverlay_New = $("#divDG_PopupOverlay" + nPopupIndex);
+
+    //빈곳을 클릭했을때 이벤트 주기
+    if (typeof jsonOpt.OverlayClick === "function")
+    {
+        divOverlay_New.click(function ()
+        {
+            jsonOpt.OverlayClick(nPopupIndex, divPopupParent_New);
+        });
+    }
+
+    //배열에 추가한다.
+    DG_Popup.List.push(divPopupParent_New);
+
+    
+
+    //완성된 개체를 리턴한다.
+    return divPopupParent;
+};
+
+/**
+ * Css의 데이터값에 단위를 제거해준다.
+ * @param {string} sData 변환할 데이터
+ * @returns {int} 변환된 값
+ */
+DG_Popup.CutBack = function (sData)
+{
+    var nReturn = 0;
+
+    if (false === isNaN(sData))
+    {//숫자형이다.
+        nReturn = Number(sData);
+    }
+    else
+    {//숫자형이 아니다.
+        //단위는 끝에 2자리이다.
+        if (2 < sData.length)
+        {//2자리 초과다.
+            //뒤에 2칸을 제외하고 받는다.
+            var sCut = sData.substring(0, sData.length - 2);
+
+            if (false === isNaN(sCut))
+            {//남은 글자가 숫자다.
+                nReturn = Number(sCut);
+            }
+        }
+    }
+
+    return nReturn;
+};
+
+/** 맨 마지막 창을 닫는다. */
+DG_Popup.Close = function ()
+{
+    //리스트 개수
+    var nIndex = DG_Popup.List.length - 1;
+
+    DG_Popup.CloseIndex(nIndex);
+};
+
+/**
+ * 지정한 인덱스의 아이템을 제거
+ * @param {int} nIndex 지울 인덱스
+ */
+DG_Popup.CloseIndex = function (nIndex)
+{
+    //팝업 개체 삭제
+    DG_Popup.List[nIndex].remove();
+
+    //리스트에서 대산 인덱스 제거
+    DG_Popup.List.splice(nIndex, 1);
+};
+
+/** 모든 창을 닫는다. */
+DG_Popup.CloseAll = function ()
+{
+    var nArrLen = DG_Popup.List.length;
+
+    for (var i = 0; i < nArrLen; ++i)
+    {
+        DG_Popup.Close();
+    }
+};
+
+/**
+ * 지정한 대상을 지운다.
+ * @param {object} objTarget 지울 대상
+ */
+DG_Popup.CloseTarget = function (objTarget)
+{
+    var nArrLen = DG_Popup.List.length;
+
+    for (var i = 0; i < nArrLen; ++i)
+    {
+        if (true === Object.is(DG_Popup.List[i], objTarget))
+        {//일치하는 오브젝트를 찾았다.
+            DG_Popup.CloseIndex(i);
+            break;
+        }
+    }
+};
